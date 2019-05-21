aaee0f0a31de733c9f8cfc120482cad475d73fb8
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-05-22 00:33:08 +0300

    localized `Register`

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,30 +1,30 @@
 <template>
     <div>
-        <h1>Registration</h1>
+        <h1>{{ $t('registration') }}</h1>
         <form @submit.prevent="register">
-            <label for="username">Username:</label>
+            <label for="username">{{ $t('username') }}:</label>
             <br>
             <input id="username" v-model="username" required autofocus>
             <br>
 
-            <label for="email">Email:</label>
+            <label for="email">{{ $t('email') }}:</label>
             <br>
             <input id="email" type="email" v-model="email" required>
             <br>
 
-            <label for="password">Password:</label>
+            <label for="password">{{ $t('password') }}:</label>
             <br>
             <input id="password" type="password" v-model="password" required>
             <br>
 
-            <label for="confirm-password">Confirm password:</label>
+            <label for="confirm-password">{{ $t('confirmPassword') }}:</label>
             <br>
             <input id="confirm-password" type="password" v-model="confirmPassword" required>
             <br>
 
             <p v-if="errorMessage">{{ errorMessage }}</p>
 
-            <button type="submit">Register</button>
+            <button type="submit">{{ $t('register') }}</button>
         </form>
     </div>
 </template>
@@ -68,6 +68,23 @@
     }
 </script>
 
-<style scoped>
-
-</style>
+<i18n>
+{
+    "en": {
+        "registration": "Registration",
+        "username": "Username",
+        "email": "Email",
+        "password": "Password",
+        "confirmPassword": "Confirm password",
+        "register": "Register"
+    },
+    "uk": {
+        "registration": "Реєстрація",
+        "username": "Юзернейм",
+        "email": "Електрона пошта",
+        "password": "Пароль",
+        "confirmPassword": "Підтвердження паролю",
+        "register": "Зареєструватися"
+    }
+}
+</i18n>
